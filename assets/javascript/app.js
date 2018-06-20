cc0ec9a4e3b78cc70cde74243d245cbd87464a8a
Alison Fahy <fahyalison@gmail.com>
2018-06-20 18:48:36 -0500

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,6 +11,21 @@ question4:if true is clicked, then inCorrect goes up by 1
             if false is clicked, then correct goes up by 1
 
 If timer === 0, and questions have not been answered, unAnswered++*/
+// const sixtySeconds = 1000 * 60;
+// let timer = 60;
+// //Timer:
+// $("#timer").append(timer);
+// // setTimeout(sixtySeconds, 1000 * 60);
+
+//   setInterval(sixtySeconds, 1000 * 60);
+//   window.onload = function() {
+//   $("#playButton").on("click", timer.start); 
+//   console.log(this);
+// }
+
+
+
+
 
 var correct = 0;
 var inCorrect = 0;
@@ -20,34 +35,72 @@ var question2 = true;
 var question3 = true;
 var question4 = false;
 
+const rightAnswers = [false, true, true, false];
+const wrongAnswers = [true, false, false, true];
+
+
+// Question 1:
 $("#q1true").on("click", function answers() {
-    // if (question1 === false) {
-    //   wins++;
-    //   $("#correct").text(correct); 
-      console.log(this.value);
-    //   answers();
-//   }
+  console.log(this.value);
 })
 
-$("#qfalse").on("click", function answers() {
-  // if (question1 === false) {
-  //   wins++;
-  //   $("#correct").text(correct); 
-    console.log(this.value);
-
+$("#q1false").on("click", function answers() {
+  console.log(this.value);
 })
+
+function addWins() {
+  // if (//user ===  {$("#q1false")
+    wins++;
+    $("#correct").text(correct);
+   
+}
+
+// else if (//user === $("#q1true")) {
+    losses++;
+    $("#inCorrect").text(inCorrect);
+  
+}
+}
+
+
+
+//Question 2:
+$("#q2true").on("click", function answers() {
+  console.log(this.value);
+})
+
+$("#q2false").on("click", function answers() {
+  console.log(this.value);
+})
+
+//Question 3:
+$("#q3true").on("click", function answers() {
+  console.log(this.value);
+})
+
+$("#q3false").on("click", function answers() {
+  console.log(this.value);
+})
+
+//Question 4:
+$("#q4true").on("click", function answers() {
+  console.log(this.value);
+})
+
+$("#q4false").on("click", function answers() {
+  console.log(this.value);
+})
+
+
+
+
+
+
 
   //question1.userproperty
   //assign this.value to that property(key) if question1.userPropert === question1.correctAnswer correct++
     //each question will be an object itself, have question id, correct answer, user guess=0
-
-//   else if (question1 === true) {
-//       losses++; {
-//       $("#losses").text(losses);
-//       resetGame();
-//   }
-//   }
-// 
+ 
 $("#correct").html(correct);
 $("#inCorrect").html(inCorrect);
 $("#unAnswered").html(unAnswered);
