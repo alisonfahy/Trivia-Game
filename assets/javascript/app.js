42e6cb8173731c7547c1f3426e49a2a5fbf2b3af
Alison Fahy <fahyalison@gmail.com>
2018-06-21 23:54:45 -0500

    final push

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,32 +1,36 @@
-/* When the "playButton" is clicked
-The timer will begin counting down.
+// $(".container").toggle();
 
-question1: if true is clicked, inCorrect++
-            else false is clicked, correct++ 
-question2: if true is clicked, correct++
-            false is clicked, inCorrect++
-question3: if true is clicked, correct++
-            false is clicked, inCorrect++
-question4:if true is clicked, then inCorrect goes up by 1
-            if false is clicked, then correct goes up by 1
+//TIMER:
+$("#playButton").on("click", function () {
+let timer = 10;
+let oneSecond = 1000;
+let timeUp = 10000;
 
-If timer === 0, and questions have not been answered, unAnswered++*/
-// const sixtySeconds = 1000 * 60;
-// let timer = 60;
-// //Timer:
-// $("#timer").append(timer);
-// // setTimeout(sixtySeconds, 1000 * 60);
-
-//   setInterval(sixtySeconds, 1000 * 60);
-//   window.onload = function() {
-//   $("#playButton").on("click", timer.start); 
-//   console.log(this);
-// }
+// $(".container").toggle();
 
 
 
+  let interval = setInterval(function () {
+    ;
+    timer--;
+    $("#timer").html("Time Remaining: " + timer);
+    if (timer === 0) clearInterval(interval);
+  }, 1000);
+  
 
 
+// $(".results").toggle();
+
+setTimeout(function() {
+  clearInterval();
+  // $(".results").show(); //you could use toggle instead of show
+  //   $(".questions").hide(); // you could use toggle instead of hide
+})
+    
+})
+
+
+  
 var correct = 0;
 var inCorrect = 0;
 var unAnswered = 4;
@@ -39,68 +43,61 @@ const rightAnswers = [false, true, true, false];
 const wrongAnswers = [true, false, false, true];
 
 
+
 // Question 1:
 $("#q1true").on("click", function answers() {
+  $("#inCorrect").html("1");
+  $("#unAnswered").html("3");
   console.log(this.value);
+
 })
 
 $("#q1false").on("click", function answers() {
+  $("#correct").html("1");
+  $("#unAnswered").html("3");
   console.log(this.value);
 })
 
-function addWins() {
-  // if (//user ===  {$("#q1false")
-    wins++;
-    $("#correct").text(correct);
-   
-}
-
-// else if (//user === $("#q1true")) {
-    losses++;
-    $("#inCorrect").text(inCorrect);
-  
-}
-}
-
-
-
 //Question 2:
 $("#q2true").on("click", function answers() {
+   $("#correct").html("2");
+  $("#unAnswered").html("2");
   console.log(this.value);
 })
 
 $("#q2false").on("click", function answers() {
+  $("#inCorrect").html("2");
+  $("#unAnswered").html("2");
   console.log(this.value);
 })
 
 //Question 3:
 $("#q3true").on("click", function answers() {
+  $("#correct").html("3");
+  $("#unAnswered").html("1");
   console.log(this.value);
 })
 
 $("#q3false").on("click", function answers() {
+  $("#inCorrect").html("3");
+  $("#unAnswered").html("1");
   console.log(this.value);
 })
 
 //Question 4:
 $("#q4true").on("click", function answers() {
+  $("#inCorrect").html("4");
+  $("#unAnswered").html("0");
   console.log(this.value);
 })
 
 $("#q4false").on("click", function answers() {
+  $("#correct").html("4");
+  $("#unAnswered").html("0");
   console.log(this.value);
 })
 
 
-
-
-
-
-
-  //question1.userproperty
-  //assign this.value to that property(key) if question1.userPropert === question1.correctAnswer correct++
-    //each question will be an object itself, have question id, correct answer, user guess=0
- 
-$("#correct").html(correct);
-$("#inCorrect").html(inCorrect);
-$("#unAnswered").html(unAnswered);
+// $("#correct").html(correct);
+// $("#inCorrect").html(inCorrect);
+// $("#unAnswered").html(unAnswered);
